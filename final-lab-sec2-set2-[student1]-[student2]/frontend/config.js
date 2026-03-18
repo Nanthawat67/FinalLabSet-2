9c3f4c915526e880f9bd3cf51b578d01818b651a
thanakon17 <thanakon.170147@gmail.com>
2026-03-18 19:11:57 +0700

    upgate frontend++

--- final-lab-sec2-set2-[student1]-[student2]/frontend/config.js
+++ final-lab-sec2-set2-[student1]-[student2]/frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_URL:     'https://your-auth-service.up.railway.app',
-  TASK_URL:     'https://your-task-service.up.railway.app',
-  ACTIVITY_URL: 'https://your-activity-service.up.railway.app'
+  AUTH_URL:     'auth-service-production-a741.up.railway.app',
+  TASK_URL:     'task-service-production-d73b.up.railway.app',
+  ACTIVITY_URL: 'activity-service-production-dabb.up.railway.app'
 };
