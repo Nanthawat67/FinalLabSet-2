50b534674a12a5679b61f44688493931b747010c
nanthawat <nan292546@gmail.com>
2026-03-19 00:34:16 +0700

    .md

--- final-lab-sec2-set2-[student1]-[student2]/frontend/config.js
+++
@@ -1,5 +0,0 @@
-window.APP_CONFIG = {
-  AUTH_URL:     'https://auth-service-production-a741.up.railway.app/',
-  TASK_URL:     'https://task-service-production-d73b.up.railway.app/',
-  ACTIVITY_URL: 'https://activity-service-production-4636.up.railway.app/'
-};
