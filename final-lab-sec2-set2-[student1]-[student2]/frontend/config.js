4a7ad6eb8f8ea34f44fcc9fd95c3d2631d9e0a38
thanakon17 <thanakon.170147@gmail.com>
2026-03-18 20:17:44 +0700

    upgate code frontend++

--- final-lab-sec2-set2-[student1]-[student2]/frontend/config.js
+++ final-lab-sec2-set2-[student1]-[student2]/frontend/config.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-window.config = {
-  AUTH_URL: "/api/auth",
-  TASK_URL: "/api/tasks",
-  ACTIVITY_URL: "/api/activity"
-=======
 window.APP_CONFIG = {
   AUTH_URL:     'auth-service-production-a741.up.railway.app',
   TASK_URL:     'task-service-production-d73b.up.railway.app',
   ACTIVITY_URL: 'activity-service-production-dabb.up.railway.app'
->>>>>>> 2a32b504d8c8a9bb85f8eb4d39246ddba845c683
 };
